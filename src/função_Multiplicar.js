8385c1c67e72b7c6bc5c985fb4661f05ebcaed8c
WilfredoSolorzano <wsolorzano00@gmail.com>
2024-06-26 18:46:23 -0300

    agregando modificaçao nos arquivos

--- src/função_Multiplicar.js
+++ src/função_Multiplicar.js
@@ -1,11 +1,9 @@
 "use strict";
 class Calculadora {
-    // Método para multiplicar dos números
     multiplicar(a, b) {
         return a * b;
     }
 }
-// Ejemplo de uso de la clase Calculadora
 const calc = new Calculadora();
-const resultadoMultiplicacao = calc.multiplicar(8, 6); // 30
-console.log(resultadoMultiplicacao); // Imprime 30
+const resultadoMultiplicacao = calc.multiplicar(8, 6);
+console.log(resultadoMultiplicacao);
--- src/função_Multiplicar.ts
+++ src/função_Multiplicar.ts
@@ -1,11 +1,10 @@
 class Calculadora {
-    // Método para multiplicar dos números
     multiplicar(a: number, b: number): number {
         return a * b;
     }
 }
 
-// Ejemplo de uso de la clase Calculadora
+
 const calc = new Calculadora();
-const resultadoMultiplicacao = calc.multiplicar(8, 6); // 30
-console.log(resultadoMultiplicacao); // Imprime 30
+const resultadoMultiplicacao = calc.multiplicar(8, 6); 
+console.log(resultadoMultiplicacao); 
